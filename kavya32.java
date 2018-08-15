55eec64145ca6e716e05f2288cc8558ef31975c4
kavyakonduri <41731461+kavyakonduri@users.noreply.github.com>
2018-08-15 13:29:51 +0530

    Create kavya32.java

---
+++ kavya32.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int number-sc.nextInt();
+        int total=0;
+        int count=0;
+        int high;
+        while(count <10)
+        {
+          total=total+number;
+          count++;
+        }
+       high=total;
+       System.out.println("max number is"+high);           
+	}
+}
